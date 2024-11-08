3dcc6bf5643f8ff6d4fb9272630423469f0b814d
muhiddin <muhiddin@gmail.com>
2024-11-08 19:04:03 +0500

    cdsds

---
+++ m.js
@@ -0,0 +1,80 @@
+// N1
+
+// function subtractNum(num) {
+//     if (num>0) {
+//         return num-2
+//     }else if(num<0){
+//         return num+1
+//     }else{
+//         return num=10
+//     }
+    
+// }
+// console.log(subtractNum(10));
+
+
+// N2
+
+// function factorial(n) {
+//    let result = 1;
+//    for (let i = 2; i <= n; i++) {
+//        result *= i;
+//    }
+//    return  result ;
+// }
+
+// console.log(factorial(5)); 
+// console.log(factorial(4)); 
+
+
+// N3
+
+// function countPies(num1,num2,num3,num4) {
+//     let nom = num1+100+num2;
+//     let nor=num3+100+num4;
+//     return Math.floor (nor/nom)
+    
+// }
+// console.log(countPies(1,70,3,80));
+
+
+// N4
+// function isPerfectNumber(num) {
+//     let sum=0;
+//     for (let i=1; i<num; i++) {
+//         if (num%i==0) {
+//             sum+=i
+            
+//         }
+//     }
+//     return sum == num;
+// }
+// console.log(isPerfectNumber(28));
+// console.log(isPerfectNumber(32));
+// console.log(isPerfectNumber(496));
+
+// N5
+
+// function nextEven(numb) {
+//     return numb -2+(numb%2);
+    
+// }
+// console.log(nextEven(5));
+// console.log(nextEven(4));
+
+// N6
+
+// function All(numb1,numb2) {
+//     while(numb2!=0){
+//         let sum=numb2;
+//         numb2=numb1%numb2;
+//         numb1=sum;
+//     }
+//     return numb1
+// }
+// console.log(All(4,6));
+// console.log(All(3,8));
+// console.log(All(2,6));
+
+
+// N7
